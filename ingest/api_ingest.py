0838ee8a01bfb0c2eac42c10c1e96f3cdb52b0a3
shardendu <appujha0@gmail.com>
2025-10-21 19:01:23 +0300

    add(etede): sensor api injest file

--- ingest/api_ingest.py
+++ ingest/api_ingest.py
@@ -1,1 +1,23 @@
 # API ingestion script
+
+import requests
+import json
+import csv
+import psycopg2
+import os
+import pytz
+from dateutil import parser
+import time
+
+host = 
+
+
+
+def sensor_api_connection():
+    pass
+
+
+
+
+
+

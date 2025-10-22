60ce66022701acf2e27130d95f2ae9991150ba78
shardendu <appujha0@gmail.com>
2025-10-22 21:00:37 +0300

    add(etede): add connection to api

--- ingest/api_ingest.py
+++ ingest/api_ingest.py
@@ -9,15 +9,35 @@ import pytz
 from dateutil import parser
 import time
 
-host = 
-
-
+host = os.environ.get("CONTAINER_IP")
+port = os.environ.get("PORT")
+database = os.environ.get("DATABASE")
+user = os.environ.get("USER")
+password = os.environ.get("PASS_WORD")
+url = os.environ.get("API_LINK")
 
 def sensor_api_connection():
-    pass
+    while True:
+        api_data_list = []
+
+        Url = url
+
+        request1 = requests.get(Url)
+        data1 = request1.json()
+        
+
+        # add_new_col = {"location": "Janonhanta1, Vantaa, Finland"}
+        # add_new_col_serial = {}
+        # data1.update(add_new_col_serial)
+        # data1.update(add_new_col)
+
+        # api_data_list.append(data1)
+
+        print(data1)
 
 
 
 
 
 
+sensor_api_connection()

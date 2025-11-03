8beec060bb7e16537b61da34cfcaa7ae38d1c99e
shardendu <appujha0@gmail.com>
2025-11-03 20:30:29 +0200

    add(etede): addazure ioy hub connection

--- ingest/api_ingest.py
+++ ingest/api_ingest.py
@@ -1,5 +1,4 @@
 # API ingestion script
-
 import requests
 import json
 import csv
@@ -7,6 +6,13 @@ import os
 import pytz
 from dateutil import parser
 import time
+import sqlite3
+from azure.iot.device import IoTHubDeviceClient, Message
+from azure.eventhub import EventHubConsumerClient
+
+primary_k_string = os.environ.get("primary_key_string")
+event_hub_endpoint = os.environ.get("event_hub_conn_str")
+event_name_of_end_point = os.environ.get("event_name")
 
 host = os.environ.get("CONTAINER_IP")
 port = os.environ.get("PORT")
@@ -19,7 +25,7 @@ url = os.environ.get("API_URL")
 
 def sensor_api_connection():
     while True:
-        api_data_list = []
+        api_data_list = {}
 
         Url = url
 
@@ -32,7 +38,90 @@ def sensor_api_connection():
         data1.update(add_new_col_serial)
         data1.update(add_new_col)
 
-        api_data_list.append(data1)
+        api_data_list.update(data1)
+
+        conn_str = primary_k_string
+
+        client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+        msg = Message(json.dumps(api_data_list))
+
+        client.send_message(msg)
+
+        conn = sqlite3.connect("/Users/shardendujha/Documents/End-to-End_Data_eng_Project/database/aiq.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS awair_data (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       timestamp TEXT,
+                       score REAL,
+                        dew_point REAL,
+                        temp REAL,
+                        humid REAL,
+                        abs_humid REAL,
+                        co2 REAL,
+                        co2_est REAL,
+                        co2_est_baseline REAL,
+                        voc REAL,
+                        voc_baseline REAL,
+                        voc_h2_raw REAL,
+                        voc_ethanol_raw REAL,
+                        pm25 REAL,
+                        pm10_est REAL
+                       )
+                       """)
+        conn.commit()
+
+        eventhub_conn_str = event_hub_endpoint
+        eventhub_name = event_name_of_end_point
+        
+        def on_event(partition_context, event):
+            try:
+                message = json.loads(event.body_as_str())
+                cursor.execute("""
+                    INSERT INTO awair_data (
+                        timestamp, score, dew_point, temp, humid, abs_humid, co2,
+                        co2_est, co2_est_baseline, voc, voc_baseline, voc_h2_raw,
+                        voc_ethanol_raw, pm25, pm10_est
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """, (
+                    message.get("timestamp"),
+                    message.get("score"),
+                    message.get("dew_point"),
+                    message.get("temp"),
+                    message.get("humid"),
+                    message.get("abs_humid"),
+                    message.get("co2"),
+                    message.get("co2_est"),
+                    message.get("co2_est_baseline"),
+                    message.get("voc"),
+                    message.get("voc_baseline"),
+                    message.get("voc_h2_raw"),
+                    message.get("voc_ethanol_raw"),
+                    message.get("pm25"),
+                    message.get("pm10_est")
+                ))
+                conn.commit()
+
+                partition_context.update_checkpoint(event)
+                print("Saved message to SQLite:", message.get("timestamp"))
+
+            except Exception as e:
+                print("Error:", e)
+
+        client = EventHubConsumerClient.from_connection_string(
+            conn_str=eventhub_conn_str,
+            consumer_group="$Default",
+            eventhub_name=eventhub_name
+        )
+
+        with client:
+            print("Listening for IoT Hub messages...")
+            client.receive(
+                on_event=on_event,
+                starting_position="@latest"  
+            )
 
         print(api_data_list)
 
@@ -62,4 +151,4 @@ sensor_api_connection()
 
 
 
-apenaq_api()
+# apenaq_api()

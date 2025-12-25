8efc53246ed66b0942a96e0fb81f6637bd94d387
shardendu <appujha0@gmail.com>
2025-12-25 18:36:03 +0200

    update(etede): update json and csv data store script

--- ingest/global_market_financial_data.py
+++ ingest/global_market_financial_data.py
@@ -1,13 +1,19 @@
 import json
-import os
-import csv
 import requests
+import os
+import csv 
 
+API_KEY = os.environ.get("marketstackapi_keys")
+url = os.environ.get("marketstackurl") # /v1/eod,/v1/tickers,/v1/exchanges,/v1/intraday
 
+csv_path = "/Users/shardendujha/Documents/End-to-End_Data_eng_Project/database/financial_market_data.csv"
+c_columns = ['open','high','low','close','volume','adj_high','adj_low','adj_close','adj_open','adj_volume',
+                 'split_factor', 'dividend','symbol','exchange', 'date']
 
 def gloabal_financial_market_data():
-    API_KEY = "5f133d5b2b64ec5f95e5d152090d3cd2"
-    url = "http://api.marketstack.com/v1/eod" # /v1/eod,/v1/tickers,/v1/exchanges,/v1/intraday
+ 
+   
+
     params = {
                 "access_key": API_KEY,
                 "symbols": "AAPL",
@@ -21,9 +27,53 @@ def gloabal_financial_market_data():
     for i in data.items():
         for row in i[1]:
             dict_data.append(row)
-        
-        print(dict_data)
-       
-            
+
+    if os.path.exists(csv_path):
+        existing_rows = []
+        with open(csv_path, "r") as read_file:
+            reader = csv.DictReader(read_file)
+
+            for row in reader:
+                existing_rows.append(row)
+
+        with open(csv_path, "a+", newline="") as add_obj:
+            writer = csv.DictWriter(add_obj, fieldnames=c_columns)
+
+
+            for row in dict_data:
+                if row in existing_rows:
+                    print(
+                        "This record already exists. You cannot make it a duplicate."
+                    )
+                else:
+                    writer.writerow(row)
+    else:
+        try:
+            with open(csv_path, "w", newline="") as write_file:
+                writer = csv.DictWriter(write_file, fieldnames=c_columns)
+                writer.writeheader()
+
+                for row_data in dict_data:
+                    writer.writerow(row_data)
+        except ValueError:
+            print("I/O Error")
+                
+    
+
+    json_path = "/Users/shardendujha/Documents/End-to-End_Data_eng_Project/database/financial_market_data.json"
+    if os.path.exists(json_path):
+        with open(json_path) as j_file:
+            existing_record = json.load(j_file)
+
+        for record in dict_data:
+            if record in existing_record:
+                print("This record is already exist. You can not make it duplicate")
+            else:
+                with open(json_path, "w") as r_json:
+                    json.dump(existing_record, r_json, sort_keys=True, indent=4)
+    else:
+        with open(json_path, "w") as write_json:
+            json.dump(dict_data, write_json, sort_keys=True, indent=4)
+
             # print(value)
 gloabal_financial_market_data()
